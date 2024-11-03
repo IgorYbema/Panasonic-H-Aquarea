ae7c5ab5c216974547b51fae11632602f4b0a314
CurlyMoo <curlymoo1@gmail.com>
2024-11-03 17:32:14 +0100

    Fix wrong condition

--- HeishaMon/HeishaMon.ino
+++ HeishaMon/HeishaMon.ino
@@ -1467,13 +1467,18 @@ void setup() {
 #if defined(ESP8266)
   rst_info *resetInfo = ESP.getResetInfoPtr();
   loggingSerial.printf(PSTR("Reset reason: %d, exception cause: %d\n"), resetInfo->reason, resetInfo->exccause);
-  if (resetInfo->reason > 0 && resetInfo->reason < 4) {
+  if (heishamonSettings->force_rules == false) {
+    if (resetInfo->reason > 0 && resetInfo->reason < 4) {
 #elif defined(ESP32)
-  esp_reset_reason_t reset_reason = esp_reset_reason(); 
-  loggingSerial.printf(PSTR("Reset reason: %d\n"), reset_reason);
-  if (heishamonSettings->force_rules == true || (reset_reason > 3 && reset_reason < 12)) {  //is this correct for esp32?
+      esp_reset_reason_t reset_reason = esp_reset_reason();
+      loggingSerial.printf(PSTR("Reset reason: %d\n"), reset_reason);
+    if (reset_reason > 3 && reset_reason < 12) {  //is this correct for esp32?
 #endif  
-    loggingSerial.println("Not loading rules due to crash reboot!");
+        loggingSerial.println("Not loading rules due to crash reboot!");
+    } else {
+      rules_parse((char *)"/rules.txt");
+      rules_boot();
+    }
   } else {
     rules_parse((char *)"/rules.txt");
     rules_boot();
